a360a6dbf2e8792506ac0afecedd1ead941a730b
llkulya <kulish_oo@ffeks.dnu.edu.ua>
2024-10-13 18:31:17 +0300

    product class

---
+++ CoffeeApp/product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace CoffeeApp
+{
+    class Product
+    {
+        public int Id {get; set;}
+        public string Coffee {get; set;} = "";
+        public double DeliveryPrice {get; set;}
+        public double SalePrice {get; set;}
+        public int Amount {get; set;}
+        public double Mass {get; set;}
+        public string CoffeeType {get; set;} = "";
+        public string MadeIn {get; set;} = "";
+        public string Composition {get; set;} = "";
+        public Image Image {get; set;}
+    }
+}
+
