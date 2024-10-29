3b547e054bdb432ae514694d98b4ce1a5c90c06a
llkulya <oleksandrkulis963@gmail.com>
2024-10-29 12:37:59 +0200

    change filter

--- CoffeeApp/FilterForm.Designer.cs
+++ CoffeeApp/FilterForm.Designer.cs
@@ -40,6 +40,8 @@
             comboBoxName = new ComboBox();
             label11 = new Label();
             panel2 = new Panel();
+            label13 = new Label();
+            label12 = new Label();
             label6 = new Label();
             numericWeightMin = new NumericUpDown();
             label7 = new Label();
@@ -51,8 +53,6 @@
             label2 = new Label();
             label1 = new Label();
             numericWeightMax = new NumericUpDown();
-            label12 = new Label();
-            label13 = new Label();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericWeightMin).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericPriceMax).BeginInit();
@@ -65,9 +65,9 @@
             label8.AutoSize = true;
             label8.Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label8.ForeColor = Color.FromArgb(82, 38, 7);
-            label8.Location = new Point(14, 325);
+            label8.Location = new Point(12, 244);
             label8.Name = "label8";
-            label8.Size = new Size(73, 29);
+            label8.Size = new Size(59, 23);
             label8.TabIndex = 11;
             label8.Text = "Тип:";
             // 
@@ -77,9 +77,10 @@
             comboBoxType.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxType.FormattingEnabled = true;
             comboBoxType.Items.AddRange(new object[] { "none" });
-            comboBoxType.Location = new Point(70, 367);
+            comboBoxType.Location = new Point(61, 275);
+            comboBoxType.Margin = new Padding(3, 2, 3, 2);
             comboBoxType.Name = "comboBoxType";
-            comboBoxType.Size = new Size(233, 25);
+            comboBoxType.Size = new Size(204, 23);
             comboBoxType.TabIndex = 12;
             // 
             // comboBoxComposition
@@ -88,9 +89,10 @@
             comboBoxComposition.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxComposition.FormattingEnabled = true;
             comboBoxComposition.Items.AddRange(new object[] { "none" });
-            comboBoxComposition.Location = new Point(70, 451);
+            comboBoxComposition.Location = new Point(61, 338);
+            comboBoxComposition.Margin = new Padding(3, 2, 3, 2);
             comboBoxComposition.Name = "comboBoxComposition";
-            comboBoxComposition.Size = new Size(233, 25);
+            comboBoxComposition.Size = new Size(204, 23);
             comboBoxComposition.TabIndex = 14;
             // 
             // label9
@@ -98,9 +100,9 @@
             label9.AutoSize = true;
             label9.Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label9.ForeColor = Color.FromArgb(82, 38, 7);
-            label9.Location = new Point(14, 409);
+            label9.Location = new Point(12, 307);
             label9.Name = "label9";
-            label9.Size = new Size(106, 29);
+            label9.Size = new Size(84, 23);
             label9.TabIndex = 13;
             label9.Text = "Склад:";
             // 
@@ -109,9 +111,10 @@
             comboBoxCountry.BackColor = Color.White;
             comboBoxCountry.FormattingEnabled = true;
             comboBoxCountry.Items.AddRange(new object[] { "none" });
-            comboBoxCountry.Location = new Point(70, 535);
+            comboBoxCountry.Location = new Point(61, 401);
+            comboBoxCountry.Margin = new Padding(3, 2, 3, 2);
             comboBoxCountry.Name = "comboBoxCountry";
-            comboBoxCountry.Size = new Size(233, 25);
+            comboBoxCountry.Size = new Size(204, 23);
             comboBoxCountry.TabIndex = 16;
             comboBoxCountry.TextUpdate += comboBoxCountry_TextUpdate;
             comboBoxCountry.Click += comboBoxCountry_Click;
@@ -122,9 +125,9 @@
             label10.AutoSize = true;
             label10.Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label10.ForeColor = Color.FromArgb(82, 38, 7);
-            label10.Location = new Point(14, 493);
+            label10.Location = new Point(12, 370);
             label10.Name = "label10";
-            label10.Size = new Size(261, 29);
+            label10.Size = new Size(209, 23);
             label10.TabIndex = 15;
             label10.Text = "Країна-виробник:";
             // 
@@ -132,38 +135,41 @@
             // 
             buttonReset.DialogResult = DialogResult.OK;
             buttonReset.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            buttonReset.Location = new Point(7, 594);
+            buttonReset.Location = new Point(6, 446);
+            buttonReset.Margin = new Padding(3, 2, 3, 2);
             buttonReset.Name = "buttonReset";
-            buttonReset.Size = new Size(158, 40);
+            buttonReset.Size = new Size(138, 30);
             buttonReset.TabIndex = 18;
             buttonReset.Text = "Скинути";
             buttonReset.UseVisualStyleBackColor = true;
             buttonReset.Click += buttonReset_Click;
+            buttonReset.MouseEnter += buttonReset_MouseEnter;
             buttonReset.MouseLeave += buttonReset_MouseLeave;
-            buttonReset.MouseHover += buttonReset_MouseHover;
             // 
             // buttonOK
             // 
             buttonOK.DialogResult = DialogResult.OK;
             buttonOK.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            buttonOK.Location = new Point(171, 594);
+            buttonOK.Location = new Point(150, 446);
+            buttonOK.Margin = new Padding(3, 2, 3, 2);
             buttonOK.Name = "buttonOK";
-            buttonOK.Size = new Size(154, 40);
+            buttonOK.Size = new Size(135, 30);
             buttonOK.TabIndex = 19;
             buttonOK.Text = "Прийняти";
             buttonOK.UseVisualStyleBackColor = true;
             buttonOK.Click += buttonOK_Click;
+            buttonOK.MouseEnter += buttonOK_MouseEnter;
             buttonOK.MouseLeave += buttonOK_MouseLeave;
-            buttonOK.MouseHover += buttonOK_MouseHover;
             // 
             // comboBoxName
             // 
             comboBoxName.BackColor = Color.White;
             comboBoxName.FormattingEnabled = true;
             comboBoxName.Items.AddRange(new object[] { "none" });
-            comboBoxName.Location = new Point(70, 283);
+            comboBoxName.Location = new Point(61, 212);
+            comboBoxName.Margin = new Padding(3, 2, 3, 2);
             comboBoxName.Name = "comboBoxName";
-            comboBoxName.Size = new Size(233, 25);
+            comboBoxName.Size = new Size(204, 23);
             comboBoxName.TabIndex = 21;
             comboBoxName.TextUpdate += comboBoxName_TextUpdate;
             comboBoxName.Click += comboBoxName_Click;
@@ -174,9 +180,9 @@
             label11.AutoSize = true;
             label11.Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label11.ForeColor = Color.FromArgb(82, 38, 7);
-            label11.Location = new Point(14, 241);
+            label11.Location = new Point(12, 181);
             label11.Name = "label11";
-            label11.Size = new Size(105, 29);
+            label11.Size = new Size(84, 23);
             label11.TabIndex = 20;
             label11.Text = "Назва:";
             // 
@@ -209,29 +215,52 @@
             panel2.Dock = DockStyle.Fill;
             panel2.Font = new Font("Vladimir Script", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
             panel2.Location = new Point(0, 0);
-            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(329, 637);
+            panel2.Size = new Size(288, 478);
             panel2.TabIndex = 23;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.FromArgb(82, 38, 7);
+            label13.Location = new Point(227, 132);
+            label13.Name = "label13";
+            label13.Size = new Size(42, 16);
+            label13.TabIndex = 23;
+            label13.Text = "грам";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label12.ForeColor = Color.FromArgb(82, 38, 7);
+            label12.Location = new Point(227, 69);
+            label12.Name = "label12";
+            label12.Size = new Size(50, 16);
+            label12.TabIndex = 22;
+            label12.Text = "₴ грн.";
+            label12.Click += label12_Click;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label6.ForeColor = Color.FromArgb(82, 38, 7);
-            label6.Location = new Point(182, 199);
+            label6.Location = new Point(159, 149);
             label6.Name = "label6";
-            label6.Size = new Size(42, 20);
+            label6.Size = new Size(32, 16);
             label6.TabIndex = 10;
             label6.Text = "До:";
             // 
             // numericWeightMin
             // 
             numericWeightMin.DecimalPlaces = 2;
-            numericWeightMin.Location = new Point(101, 199);
+            numericWeightMin.Location = new Point(88, 149);
+            numericWeightMin.Margin = new Padding(3, 2, 3, 2);
             numericWeightMin.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
             numericWeightMin.Name = "numericWeightMin";
-            numericWeightMin.Size = new Size(74, 26);
+            numericWeightMin.Size = new Size(65, 22);
             numericWeightMin.TabIndex = 7;
             numericWeightMin.ValueChanged += numericWeightMin_ValueChanged;
             // 
@@ -240,9 +269,9 @@
             label7.AutoSize = true;
             label7.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label7.ForeColor = Color.FromArgb(82, 38, 7);
-            label7.Location = new Point(45, 199);
+            label7.Location = new Point(39, 149);
             label7.Name = "label7";
-            label7.Size = new Size(53, 20);
+            label7.Size = new Size(39, 16);
             label7.TabIndex = 9;
             label7.Text = "Від :";
             // 
@@ -251,19 +280,20 @@
             label5.AutoSize = true;
             label5.Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label5.ForeColor = Color.FromArgb(82, 38, 7);
-            label5.Location = new Point(8, 157);
+            label5.Location = new Point(7, 118);
             label5.Name = "label5";
-            label5.Size = new Size(92, 29);
+            label5.Size = new Size(73, 23);
             label5.TabIndex = 6;
             label5.Text = "Маса:";
             // 
             // numericPriceMax
             // 
             numericPriceMax.DecimalPlaces = 2;
-            numericPriceMax.Location = new Point(234, 115);
+            numericPriceMax.Location = new Point(205, 86);
+            numericPriceMax.Margin = new Padding(3, 2, 3, 2);
             numericPriceMax.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
             numericPriceMax.Name = "numericPriceMax";
-            numericPriceMax.Size = new Size(69, 26);
+            numericPriceMax.Size = new Size(60, 22);
             numericPriceMax.TabIndex = 3;
             numericPriceMax.ValueChanged += numericPriceMax_ValueChanged;
             // 
@@ -272,19 +302,20 @@
             label4.AutoSize = true;
             label4.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label4.ForeColor = Color.FromArgb(82, 38, 7);
-            label4.Location = new Point(182, 115);
+            label4.Location = new Point(159, 86);
             label4.Name = "label4";
-            label4.Size = new Size(42, 20);
+            label4.Size = new Size(32, 16);
             label4.TabIndex = 5;
             label4.Text = "До:";
             // 
             // numericPriceMin
             // 
             numericPriceMin.DecimalPlaces = 2;
-            numericPriceMin.Location = new Point(101, 115);
+            numericPriceMin.Location = new Point(88, 86);
+            numericPriceMin.Margin = new Padding(3, 2, 3, 2);
             numericPriceMin.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
             numericPriceMin.Name = "numericPriceMin";
-            numericPriceMin.Size = new Size(74, 26);
+            numericPriceMin.Size = new Size(65, 22);
             numericPriceMin.TabIndex = 2;
             numericPriceMin.ValueChanged += numericPriceMin_ValueChanged;
             // 
@@ -293,9 +324,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label3.ForeColor = Color.FromArgb(82, 38, 7);
-            label3.Location = new Point(45, 115);
+            label3.Location = new Point(39, 86);
             label3.Name = "label3";
-            label3.Size = new Size(53, 20);
+            label3.Size = new Size(39, 16);
             label3.TabIndex = 4;
             label3.Text = "Від :";
             // 
@@ -304,9 +335,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label2.ForeColor = Color.FromArgb(82, 38, 7);
-            label2.Location = new Point(14, 73);
+            label2.Location = new Point(12, 55);
             label2.Name = "label2";
-            label2.Size = new Size(84, 29);
+            label2.Size = new Size(68, 23);
             label2.TabIndex = 1;
             label2.Text = "Ціна:";
             // 
@@ -315,52 +346,31 @@
             label1.AutoSize = true;
             label1.Font = new Font("Verdana", 15.75F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 204);
             label1.ForeColor = Color.FromArgb(82, 38, 7);
-            label1.Location = new Point(57, 12);
+            label1.Location = new Point(50, 9);
             label1.Name = "label1";
-            label1.Size = new Size(206, 32);
+            label1.Size = new Size(162, 25);
             label1.TabIndex = 0;
             label1.Text = "Фільтрувати";
             // 
             // numericWeightMax
             // 
             numericWeightMax.DecimalPlaces = 2;
-            numericWeightMax.Location = new Point(234, 199);
+            numericWeightMax.Location = new Point(205, 149);
+            numericWeightMax.Margin = new Padding(3, 2, 3, 2);
             numericWeightMax.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
             numericWeightMax.Name = "numericWeightMax";
-            numericWeightMax.Size = new Size(69, 26);
+            numericWeightMax.Size = new Size(60, 22);
             numericWeightMax.TabIndex = 8;
             numericWeightMax.ValueChanged += numericWeightMax_ValueChanged;
             // 
-            // label12
-            // 
-            label12.AutoSize = true;
-            label12.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label12.ForeColor = Color.FromArgb(82, 38, 7);
-            label12.Location = new Point(259, 92);
-            label12.Name = "label12";
-            label12.Size = new Size(66, 20);
-            label12.TabIndex = 22;
-            label12.Text = "₴ грн.";
-            label12.Click += label12_Click;
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label13.ForeColor = Color.FromArgb(82, 38, 7);
-            label13.Location = new Point(259, 176);
-            label13.Name = "label13";
-            label13.Size = new Size(55, 20);
-            label13.TabIndex = 23;
-            label13.Text = "грам";
-            // 
             // FilterForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(329, 637);
+            ClientSize = new Size(288, 478);
             Controls.Add(panel2);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             Name = "FilterForm";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Фільтр";
--- CoffeeApp/RegisterForm.Designer.cs
+++ CoffeeApp/RegisterForm.Designer.cs
@@ -42,98 +42,109 @@
             // 
             // textBoxPass
             // 
-            textBoxPass.Location = new Point(10, 174);
+            textBoxPass.Location = new Point(9, 157);
+            textBoxPass.Margin = new Padding(3, 2, 3, 2);
             textBoxPass.MaxLength = 32;
             textBoxPass.Name = "textBoxPass";
             textBoxPass.PasswordChar = '*';
-            textBoxPass.Size = new Size(282, 27);
+            textBoxPass.Size = new Size(247, 23);
             textBoxPass.TabIndex = 0;
             // 
             // textBoxPassCheck
             // 
-            textBoxPassCheck.Location = new Point(10, 250);
+            textBoxPassCheck.Location = new Point(12, 229);
+            textBoxPassCheck.Margin = new Padding(3, 2, 3, 2);
             textBoxPassCheck.MaxLength = 32;
             textBoxPassCheck.Name = "textBoxPassCheck";
             textBoxPassCheck.PasswordChar = '*';
-            textBoxPassCheck.Size = new Size(282, 27);
+            textBoxPassCheck.Size = new Size(247, 23);
             textBoxPassCheck.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(10, 9);
+            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(35, 9);
             label1.Name = "label1";
-            label1.Size = new Size(282, 56);
+            label1.Size = new Size(292, 44);
             label1.TabIndex = 2;
             label1.Text = "Вітаємо новий адміністратор.\r\nЗареєструйтесь в системі!\r\n";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // buttonCancel
             // 
+            buttonCancel.BackColor = SystemColors.ActiveCaption;
             buttonCancel.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            buttonCancel.Location = new Point(12, 300);
+            buttonCancel.Location = new Point(48, 287);
+            buttonCancel.Margin = new Padding(3, 2, 3, 2);
             buttonCancel.Name = "buttonCancel";
-            buttonCancel.Size = new Size(124, 42);
+            buttonCancel.Size = new Size(115, 41);
             buttonCancel.TabIndex = 3;
             buttonCancel.Text = "Вийти";
-            buttonCancel.UseVisualStyleBackColor = true;
+            buttonCancel.UseVisualStyleBackColor = false;
             buttonCancel.Click += buttonCancel_Click;
             // 
             // buttonLogIn
             // 
+            buttonLogIn.BackColor = SystemColors.ActiveCaption;
             buttonLogIn.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            buttonLogIn.Location = new Point(151, 300);
+            buttonLogIn.Location = new Point(198, 287);
+            buttonLogIn.Margin = new Padding(3, 2, 3, 2);
             buttonLogIn.Name = "buttonLogIn";
-            buttonLogIn.Size = new Size(127, 42);
+            buttonLogIn.Size = new Size(115, 41);
             buttonLogIn.TabIndex = 4;
             buttonLogIn.Text = "Увійти";
-            buttonLogIn.UseVisualStyleBackColor = true;
+            buttonLogIn.UseVisualStyleBackColor = false;
             buttonLogIn.Click += buttonLogIn_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 10F);
-            label2.Location = new Point(10, 151);
+            label2.BackColor = SystemColors.ActiveCaption;
+            label2.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(9, 136);
             label2.Name = "label2";
-            label2.Size = new Size(133, 23);
+            label2.Size = new Size(114, 16);
             label2.TabIndex = 5;
             label2.Text = "Введіть пароль:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 10F);
-            label3.Location = new Point(10, 224);
+            label3.BackColor = SystemColors.ActiveCaption;
+            label3.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 208);
             label3.Name = "label3";
-            label3.Size = new Size(154, 23);
+            label3.Size = new Size(128, 16);
             label3.TabIndex = 6;
             label3.Text = "Повторіть пароль:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 10F);
-            label4.Location = new Point(10, 87);
+            label4.BackColor = SystemColors.ActiveCaption;
+            label4.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(12, 67);
             label4.Name = "label4";
-            label4.Size = new Size(116, 23);
+            label4.Size = new Size(99, 16);
             label4.TabIndex = 8;
             label4.Text = "Введіть логін:";
             // 
             // textBoxLogin
             // 
-            textBoxLogin.Location = new Point(10, 110);
+            textBoxLogin.Location = new Point(12, 88);
+            textBoxLogin.Margin = new Padding(3, 2, 3, 2);
             textBoxLogin.MaxLength = 32;
             textBoxLogin.Name = "textBoxLogin";
-            textBoxLogin.Size = new Size(282, 27);
+            textBoxLogin.Size = new Size(247, 23);
             textBoxLogin.TabIndex = 7;
             // 
             // RegisterForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(304, 354);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(361, 354);
             Controls.Add(label4);
             Controls.Add(textBoxLogin);
             Controls.Add(label3);
@@ -144,6 +155,7 @@
             Controls.Add(textBoxPassCheck);
             Controls.Add(textBoxPass);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             Name = "RegisterForm";
             Text = "Реєстрація";
             ResumeLayout(false);
